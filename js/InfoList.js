e8befc15ca215f11ea5e95343c4c2cb57d970829
tictaclu <tictaclu@localhost.localdomain>
2016-07-13 10:38:47 +0800

    update post frontend

--- js/InfoList.js
+++ js/InfoList.js
@@ -8,10 +8,14 @@ $scope.pageSize = 5;
 $scope.pageContent = [];
 $scope.currentPage = 1;
 $scope.pageUnderIndex = [1,2,3,4,5];
-
+$scope.editModel = {};
 
 //start function here
-
+$scope.bindDataEdit = function (html) {
+  // body...
+var item = $scope.pageContent[html.$index];
+$scope.editModel = item;
+};
 //FUnction to reach Service and fetch data by page Index
 $scope.GetListService = function(argument) {
   console.log("1");
@@ -31,13 +35,16 @@ identifier++;
 };
 $scope.GetRequisitionByIndex();
 
+
+
+
 $scope.PrePage = function (argument) {
 // To update the index sequence with we are clicking pre page. $scope.currentPage - 1 != 0 is to prevent from go to -1 page
   if($scope.currentPage == $scope.pageUnderIndex[0] && $scope.currentPage - 1 != 0){
     for(x in $scope.pageUnderIndex){
       $scope.pageUnderIndex[x] = $scope.pageUnderIndex[x]-1;
-    }
   }
+}
 
 //To secure when we at the frist page, we won't go to -1 page
 if($scope.currentPage - 1 != 0){
@@ -62,9 +69,29 @@ $scope.NextPage = function (argument) {
   $scope.pageIndex = $scope.pageIndex + 1;
   $scope.GetListService();
   }
-
 };
 
+
+
+$scope.CreateNewRequisition = function () {
+    RequisitionService.CreateNewRequisition().then(function(argument) {
+    console.log(argument);
+  });
+};
+
+
+$scope.EditRecord = function () {
+  RequisitionService.UpdateItem($scope.editModel).then(function(response){
+  if(response.data){
+  $scope.status = "successfully update";
+  }
+  });
+};
+
+$scope.close = function (argument) {
+  // body...
+$scope.status = "";
+};
 // function ended
 });
 
@@ -76,6 +103,34 @@ this.GetRequisitionByIndex = function(pageIndex, pageSize){
   var request = $http.get(address);
    return request;
 };
+
+this.CreateNewRequisition = function () {
+  // body...
+  var value = {
+   RequestedBy : "lujeng",
+   SRNo : "SRNo---000010",
+   SRType : 123,
+   SRStatusID : 4,
+   Feedback:"good",
+   RequestDate : "20160501",
+   ShopName : "se.ShopName",
+   Remark : "se.Remark",
+   Email : "se.Email",
+   ContactNumber : "123456"
+   };
+  var data = JSON.stringify(value);
+  var address = "http://192.168.0.11/WestCoastStreetService.svc/CreateNewRequisition";
+  var request = $http.post(address,data);
+        return request;
+};
+this.UpdateItem = function (argument) {
+  // body...
+    var data = JSON.stringify(argument);
+  var address = "http://192.168.0.11/WestCoastStreetService.svc/UpdateRequisition";
+  var request = $http.post(address,data);
+        return request;
+};
+
 
 // function ended
 });
